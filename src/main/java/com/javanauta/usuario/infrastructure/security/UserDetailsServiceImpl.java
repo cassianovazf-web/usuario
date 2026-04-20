a24328321f414332339c0103778c2346a5086b62
cassianovazf-web <cassianovazf@gmail.com>
2026-04-20 19:06:15 -0300

    adicionado o método que salva dados de usuário

--- src/main/java/com/javanauta/usuario/infrastructure/security/UserDetailsServiceImpl.java
+++ src/main/java/com/javanauta/usuario/infrastructure/security/UserDetailsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.javanauta.usuario.infrastructure.security;
 
 
+import com.javanauta.usuario.infrastructure.entity.Usuario;
+import com.javanauta.usuario.infrastructure.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
